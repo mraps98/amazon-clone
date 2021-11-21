48155d9ec9e3af8bff5350c8c2bd826873e0e832
Amritpreet Singh <amritpreet98@gmail.com>
2021-11-20 18:31:09 -0600

    feat: header

---
+++ components/Header/index.tsx
@@ -0,0 +1,55 @@
+import Image from 'next/image';
+import {
+  SearchIcon,
+  MenuIcon,
+  ShoppingCartIcon,
+} from '@heroicons/react/outline';
+import { FC } from 'react';
+
+const Header: FC = () => {
+  return (
+    <header className="sticky top-0">
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            alt="Amazon"
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
+
+        <div className="hidden sm:flex items-center h-10 rounded-md bg-yellow-400 flex-grow hover:bg-yellow-500 cursor-pointer">
+          <input
+            type="text"
+            className="p-2 h-full w-6 flex-grow rounded-l-md flex-shrink focus:outline-none px-4"
+          />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="link">
+            <p>Hello Amritpreet Singh</p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
+          </div>
+          <div className="link">
+            <p>Returns</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+          <div className="relative link flex items-center">
+            <span className="rounded-full text-black text-center font-bold bg-yellow-400 absolute top-0 right-0 md:right-6 h-4 w-4">
+              0
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="font-extrabold md:text-sm hidden md:inline">Cart</p>
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </header>
+  );
+};
+
+export default Header;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['links.papareact.com', 'fakestoreapi.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
+};
