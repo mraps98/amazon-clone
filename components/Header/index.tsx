e435cffcf83db081c86323c3464a369a505080ac
Amritpreet Singh <amritpreet98@gmail.com>
2021-11-20 18:36:55 -0600

    feat(header): bottom links

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -47,7 +47,21 @@ const Header: FC = () => {
           </div>
         </div>
       </div>
-      <div></div>
+
+      <div className="space-x-3 p-2 pl-6 flex items-center bg-amazon_blue-light text-white text-sm">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" /> All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link hidden lg:inline-flex">Electronics</p>
+        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
+      </div>
     </header>
   );
 };
