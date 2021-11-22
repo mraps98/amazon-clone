c45322ffd64e9a077676e3ccb8749eadc3d081a7
Amritpreet Singh <amritpreet98@gmail.com>
2021-11-21 19:08:00 -0600

    feat: checkoutpage

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -9,8 +9,10 @@ import {
 } from '../redux/reducers/cart';
 import CheckoutProduct from '../components/CheckoutProduct';
 import Currency from 'react-currency-formatter';
+import { useSession } from 'next-auth/client';
 
 const Checkout = () => {
+  const [session] = useSession();
   const cartItems: Array<ICartReducerCartItem> = useSelector(selectItems);
   const numberOfItemsInCart = useSelector(selectNumberOfItems);
   const cartTotalAmount = useSelector(selectTotal);
@@ -42,9 +44,27 @@ const Checkout = () => {
         </div>
 
         {/* Right */}
-        <div>
-          Subtotal ({numberOfItemsInCart} items):{' '}
-          <Currency quantity={cartTotalAmount} />
+        <div className="flex flex-col bg-white p-10 shadow-md lg:h-52">
+          {numberOfItemsInCart > 0 && (
+            <>
+              <h2>
+                Subtotal ({numberOfItemsInCart} items):{' '}
+                <span className="font-bold">
+                  <Currency quantity={cartTotalAmount} />
+                </span>
+              </h2>
+
+              <button
+                disabled={!session}
+                className={`button mt-2 ${
+                  !session &&
+                  'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'
+                }`}
+              >
+                {session ? 'Proceed to checkout' : 'Sign in to checkout'}
+              </button>
+            </>
+          )}
         </div>
       </main>
     </div>
